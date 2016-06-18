819a3042131748663e43e779e2654246f966aef5
jiasm <jiashunming@outlook.com>
2016-06-18 18:30:58 +0800

    todo startsWith

---
+++ With/startsWith.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * 返回当前字符串是否由给定的字符串为开头
+ * @param  {String} str     判断是否为该字符串为开头
+ * @param  {Number} postion 开头的位置 默认为0
+ * @return {Boolean}        true： 是以该字符串为开头的
+ */
+function startsWith(str, position) {
+
+  position = +position | 0;
+}
+
+startsWith('', '');
